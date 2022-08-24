b69b07382f55835310495ee8e9b33d53e1b47ca6
Elijah Sink <esink@fromyouflowers.com>
2022-08-24 16:27:16 -0400

    Update configs for fyf mac

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,8 +6,6 @@ alias git-stat-all='find . -type d -name .git -exec sh -c "cd \"{}\"/../ && prin
 alias git-pull-all='find . -type d -name .git -exec sh -c "cd \"{}\"/../ && printf \"\n\" && pwd && git pull && printf \"\n\"" \;'
 alias git-clean-all='find . -type d -name .git -exec sh -c "cd \"{}\"/../ && printf \"\n\" && pwd && git remote prune origin && git branch --merged | egrep -v \"(^\*|master|dev)\" | xargs git branch -d && printf \"\n\"" \;'
 
-alias feh='feh --scale-down --auto-zoom'  # open images in feh scaled to window size
-
 alias cp='cp -i'  # confirm before overwriting something
 alias df='df -h'  # human-readable sizes
 alias du='du -h'  # ditto
@@ -16,26 +14,9 @@ alias rm='rm -iv'  # confirm before deletion
 alias c=clear  # shortcut to clear screen
 
 export PATH="$PATH:/home/ejsink/.local/bin"
-# export PATH="$PATH:/usr/local/anaconda/bin"
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-# eval /usr/local/anaconda/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
 
 # autojump
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
-# X server
-export DISPLAY=(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
-export LIBGL_ALWAYS_INDIRECT=1
+export NVM_DIR="$HOME/.nvm"
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/ejsink/installs/google-cloud-sdk/path.fish.inc' ]; . '/home/ejsink/installs/google-cloud-sdk/path.fish.inc'; end
-
-# pdm
-if test -n "$PYTHONPATH"
-    set -x PYTHONPATH '/home/ejsink/.local/pipx/venvs/pdm/lib/python3.9/site-packages/pdm/pep582' $PYTHONPATH
-else
-    set -x PYTHONPATH '/home/ejsink/.local/pipx/venvs/pdm/lib/python3.9/site-packages/pdm/pep582'
-end
